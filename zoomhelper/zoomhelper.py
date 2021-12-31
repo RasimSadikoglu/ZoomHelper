7f98a0bf835c063f4f3f6ca6612501916fe976a4
RasimSadikoglu <sadikoglurasim@gmail.com>
2021-12-31 15:10:27 +0300

    decrypt data file

--- zoomhelper/zoomhelper.py
+++ zoomhelper/zoomhelper.py
@@ -1,8 +1,32 @@
-import data, sys, time
+import data, sys, time, itertools, os
 from interface import Interface
 
-def run():
+def decrypt():
+    if not os.path.exists(f'{sys.path[0]}/../files/cdata') or os.path.exists(f'{sys.path[0]}/../files/data.json'):
+        return
+    
+    try:
+        with open(f'{sys.path[0]}/../files/cdata', 'rb') as inputF:
+            data = inputF.read()
+        inputF.close()
 
+        password = input('Encryted data file found please enter the password: ')
+        
+        password = bytes(password, 'utf-8')
+
+        data = bytes([(int(d) - int(p) + 256) % 256 for p, d in zip(itertools.cycle(password), data)])
+
+        with open(f'{sys.path[0]}/../files/data.json', 'w') as out:
+            out.write(data.decode())
+    except KeyboardInterrupt:
+        raise KeyboardInterrupt()
+    except:
+        os.remove(f'{sys.path[0]}/../files/data.json')
+        print('Wrong password!')
+    else:
+        print('Data file decrypted!')
+
+def run():
     config = data.readConfigFile()
     startTimeOffset, endTimeOffset = config['startTimeOffset'], config['endTimeOffset']
 
@@ -15,6 +39,7 @@ def run():
         meetings[0].open()
 
 if __name__ == '__main__':
+    decrypt()
 
     if len(sys.argv) == 1:
         run()
