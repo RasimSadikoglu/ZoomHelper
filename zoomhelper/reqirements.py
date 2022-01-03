a1a24866cf26362240c9eab90605d804d033ceb0
RasimSadikoglu <sadikoglurasim@gmail.com>
2022-01-03 20:27:30 +0300

    sys.exec

--- zoomhelper/reqirements.py
+++ zoomhelper/reqirements.py
@@ -1,7 +1,8 @@
 import subprocess, sys, time
 
 def install():
-    subprocess.check_call(['pip', 'install', '-r', f'{sys.path[0]}/requirements.txt'])
+
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', f'{sys.path[0]}/requirements.txt'])
     print('Please restart the program!')
     time.sleep(3)
     exit(0)
