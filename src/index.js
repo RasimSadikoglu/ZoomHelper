1eb02db2e84b89d1af87ac9ff5bc9767d16d6d20
RasimSadikoglu <sadikoglurasim@gmail.com>
2022-01-25 20:59:18 +0300

    grid

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const {app, BrowserWindow} = require('electron');
+
+app.on('ready', () => {
+
+    const mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 600,
+        title: 'Meeting Helper',
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+    mainWindow.webContents.toggleDevTools();
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+});
